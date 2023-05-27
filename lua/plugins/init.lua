0ca7c74928712568e7d6a9156cbf2634c857e469
David Friar <david.friar@gmail.com>
2023-05-27 23:14:43 +0100

    flit config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,7 +23,9 @@ return {
     "ggandor/flit.nvim",
     dependencies = "ggandor/leap.nvim",
     config = function()
-      require("flit").setup({})
+      require("flit").setup({
+        labeled_modes = "nv",
+      })
     end,
   },
   { "mbbill/undotree" },
