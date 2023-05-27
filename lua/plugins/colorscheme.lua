baf77a0770aa75c700680ebaf579b059051e3ab7
David Friar <david.friar@gmail.com>
2023-05-27 23:15:58 +0100

    add Nord

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,16 @@
 return {
-  "luisiacc/gruvbox-baby",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.gruvbox_baby_function_style = "NONE"
-    vim.g.gruvbox_baby_keyword_style = "italic"
-    --vim.g.gruvbox_baby_telescope_theme = 1
-    vim.cmd([[colorscheme gruvbox-baby]])
-  end,
+  {
+    "luisiacc/gruvbox-baby",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_baby_function_style = "NONE"
+      vim.g.gruvbox_baby_keyword_style = "italic"
+      --vim.g.gruvbox_baby_telescope_theme = 1
+      vim.cmd([[colorscheme gruvbox-baby]])
+    end,
+  },
+  {
+    "shaunsingh/nord.nvim",
+  },
 }
