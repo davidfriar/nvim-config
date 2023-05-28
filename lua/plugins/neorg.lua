321557731f505d3a2dbec222ec48c0d6b386ac2c
David Friar <david.friar@gmail.com>
2023-05-28 19:04:26 +0100

    keybinding for toggle concealer

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,30 +1,40 @@
 return {
   "nvim-neorg/neorg",
   build = ":Neorg sync-parsers",
+  dependencies = { { "nvim-lua/plenary.nvim" } },
   lazy = true,
   ft = "norg",
-  opts = {
-    load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.concealer"] = {
-        config = {
-          folds = false,
-          icon_preset = "diamond",
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {
+          config = {
+            folds = false,
+            icon_preset = "diamond",
+          },
         },
-      },
-      ["core.dirman"] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            mynotes = "~/notes",
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              mynotes = "~/notes",
+            },
+          },
+        },
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+        ["core.keybinds"] = {
+          config = {
+            hook = function(keybinds)
+              -- Binds the `gtd` key in `norg` mode to execute `:echo 'Hello'`
+              keybinds.map("norg", "n", ",,", "<cmd>Neorg toggle-concealer<CR>")
+            end,
           },
         },
       },
-      ["core.completion"] = {
-        config = {
-          engine = "nvim-cmp",
-        },
-      },
-    },
-  },
-  dependencies = { { "nvim-lua/plenary.nvim" } },
+    })
+  end,
 }
