b7ece02912a5769a25cdfa0ad7ee8369c19fac72
David Friar <david.friar@gmail.com>
2023-05-28 12:22:38 +0100

    signature help

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -4,6 +4,7 @@ return {
   event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
@@ -36,6 +37,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
         { name = "neorg" },
         { name = "buffer" },
         { name = "path" },
