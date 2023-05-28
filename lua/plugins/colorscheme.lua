81589a49f9daa8ee05c0d021144c7eb469181c44
David Friar <david.friar@gmail.com>
2023-05-28 19:06:40 +0100

    fix html highlighting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,10 @@ return {
       local colors = require("gruvbox-baby.colors").config()
       vim.g.gruvbox_baby_function_style = "NONE"
       vim.g.gruvbox_baby_keyword_style = "italic"
-      vim.g.gruvbox_baby_highlights = { ["@text.uri"] = { style = "NONE", fg = colors.light_blue } }
+      vim.g.gruvbox_baby_highlights = {
+        ["@text.uri"] = { style = "NONE", fg = colors.light_blue },
+        ["@tag.delimiter"] = { style = "NONE", fg = colors.clean_green },
+      }
       --vim.g.gruvbox_baby_telescope_theme = 1
       vim.cmd([[colorscheme gruvbox-baby]])
     end,
