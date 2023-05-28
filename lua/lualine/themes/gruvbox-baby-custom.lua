52d0aa300375fce3d5e3104751c858c82e9d645e
David Friar <david.friar@gmail.com>
2023-05-28 19:07:37 +0100

    custom gruvbox-baby theme for lualine

---
+++ lua/lualine/themes/gruvbox-baby-custom.lua
@@ -0,0 +1,38 @@
+local c = require("gruvbox-baby.colors").config()
+
+local M = {
+  normal = {
+    a = { bg = c.light_blue, fg = c.dark, gui = "bold" },
+    b = { bg = c.medium_gray, fg = c.light_blue },
+    c = { bg = c.background, fg = c.milk },
+  },
+  insert = {
+    a = { bg = c.bright_yellow, fg = c.dark, gui = "bold" },
+    b = { bg = c.medium_gray, fg = c.bright_yellow },
+    c = { bg = c.background, fg = c.milk },
+  },
+  visual = {
+    a = { bg = c.milk, fg = c.dark, gui = "bold" },
+    b = { bg = c.medium_gray, fg = c.milk },
+    c = { bg = c.background, fg = c.milk },
+  },
+  replace = {
+    a = { bg = c.error_red, fg = c.dark, gui = "bold" },
+    b = { bg = c.medium_gray, fg = c.error_red },
+    c = { bg = c.background, fg = c.milk },
+  },
+  command = {
+    a = { bg = c.soft_green, fg = c.dark, gui = "bold" },
+    b = { bg = c.medium_gray, fg = c.soft_green },
+    c = { bg = c.background, fg = c.milk },
+  },
+  inactive = {
+    a = { bg = c.dark_gray, fg = c.gray, gui = "bold" },
+    b = { bg = c.medium_gray, fg = c.gray },
+    c = { bg = c.dark_gray, fg = c.gray },
+  },
+}
+
+M.terminal = M.insert
+
+return M
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,24 +2,31 @@ return {
   "nvim-lualine/lualine.nvim",
   name = "lualine",
   event = "VeryLazy",
-  opts = {
-    -- See :help lualine.txt
-    options = {
-      theme = "gruvbox-baby",
-      icons_enabled = true,
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {
-        statusline = { "NvimTree", "alpha" },
-      },
-    },
-    sections = {
-      lualine_c = {
-        { "filename", path = 1 },
-      },
-    },
-  },
   init = function()
     vim.opt.showmode = false
+  end,
+  config = function()
+    require("lualine").setup({
+      -- See :help lualine.txt
+      options = {
+        theme = "gruvbox-baby-custom",
+        icons_enabled = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = { "NvimTree", "alpha" },
+        },
+      },
+      sections = {
+        lualine_c = {
+          { "filename", path = 1 },
+        },
+        lualine_x = { "filetype"
+ },
+        lualine_y = { "fileformat", "encoding"
+ },
+        lualine_z = { "progress", "location" },
+      },
+    })
   end,
 }
