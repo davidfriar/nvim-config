63703a80d909a5e9c7f4a056fb4231a871e53825
David Friar <david.friar@cognifide.com>
2023-05-29 17:16:40 +0100

    workaround for Neorg concealer bug

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,1 +1,8 @@
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = [[ if &ft ==# 'help' | wincmd L | endif]] })
+
+local function refreshConcealer()
+  vim.cmd("Neorg toggle-concealer")
+  vim.cmd("Neorg toggle-concealer")
+end
+
+vim.api.nvim_create_autocmd("BufEnter", { pattern = "*.norg", callback = refreshConcealer })
