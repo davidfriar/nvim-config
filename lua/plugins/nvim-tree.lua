8216c3e269c4d4ea33bd165688b13d669359c712
David Friar <david.friar@gmail.com>
2023-05-29 08:07:11 +0100

    open nvim tree for dirs

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,24 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  lazy = true,
-  cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+  -- lazy = true,
+  -- cmd = { "NvimTreeToggle", "NvimTreeFocus" },
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local function open_nvim_tree(data)
+      local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+      local directory = vim.fn.isdirectory(data.file) == 1
+      if not no_name and not directory then
+        return
+      end
+      if directory then
+        vim.cmd.cd(data.file)
+      end
+      require("nvim-tree.api").tree.open()
+    end
+    vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
     require("nvim-tree").setup({})
   end,
 }
--- lua/plugins/start.lua
+++ lua/plugins/start.lua
@@ -16,9 +16,10 @@ return {
   config = function()
     local theme = require("alpha.themes.dashboard")
     theme.section.buttons.val = {
-      theme.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      theme.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
       theme.button("f", "󰈞  Find files", ":Telescope find_files <CR>"),
       theme.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+      theme.button("e", "󰙅  Explore", ":e .<CR>"),
       theme.button("q", "  Quit NVIM", ":qa<CR>"),
     }
 
